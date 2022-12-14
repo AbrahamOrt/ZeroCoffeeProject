a207a2d0c570107e1ebda50b15873f79b668fa17
Chris <chrisabner@hotmail.com>
2022-12-13 22:52:19 -0600

    FireBase google-services.json funcionando, direccion de api routes ApiRoutes.kt  y enlace de MercadoPagoRoutes.kt

--- app/src/main/java/com/optic/deliverykotlinudemy/routes/MercadoPagoRoutes.kt
+++ app/src/main/java/com/optic/deliverykotlinudemy/routes/MercadoPagoRoutes.kt
@@ -11,9 +11,9 @@ import retrofit2.http.Query
 
 interface MercadoPagoRoutes {
 
-    @GET("v1/payment_methods/installments?access_token="YOUR_FIRST_API_KEY_FROM_MERCADOPEGADO")
+    @GET("v1/payment_methods/installments?access_token=TEST-3079885896947439-121400-51eac942a27628b3def14211bc82ee01-494480056")
     fun getInstallments(@Query("bin") bin: String, @Query("amount") amount: String): Call<JsonArray>
 
-    @POST("v1/card_tokens?public_key="YOUR_SECOND_API_KEY_FROM_MERCADO_PAGO")
+    @POST("v1/card_tokens?public_key=TEST-40a80dbc-df47-45ba-8951-5f197c7199c0")
     fun createCardToken(@Body body: MercadoPagoCardTokenBody): Call<JsonObject>
 }
